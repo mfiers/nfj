f52c4f89d17caa4ebf2a6295dfe68972831a5049
Mark Fiers <mark.fiers.42@gmail.com>
2016-04-27 14:24:49 +0200

    initial working version

--- nfj/plugin/gtf.py
+++ nfj/plugin/gtf.py
@@ -50,19 +50,29 @@ def load_gtf(app, args):
     del exons['attrs']
     
     # generate a per gene collection of exons, junctions & biotypes
-    for geneid in ['ENSMUSG00000033845']:
+    junction_effect_table = []
+    
+    for _, generec in genes.iterrows():
+        gene_id = generec['gene_id']
+        gene_name = generec['gene_name']
         xtable = {}
         jtable = {}
         btypes = defaultdict(lambda: set())
-        xt = exons[exons['gene_id'] == geneid]
+        xt = exons[exons['gene_id'] == gene_id]
         for transid in xt['transcript_id'].unique():
             xtt = xt[xt['transcript_id'] == transid]
+            if len(xtt) == 1:
+                #one exon transcript - no junctions - not interseting
+                continue
             biotype = list(xtt['transcript_biotype'].unique())                
             assert len(biotype) == 1
             biotype = biotype[0]
-            exon_coords = sorted([tuple(sorted((x['start'],x['stop'])))
-                                  for (nx, x) in xtt.iterrows()])
+            xtt = xtt.sort_values(by=['chr', 'start', 'stop'])
+            exon_coords = [(x['chr'], x['start'], x['stop'])
+                           for (nx, x) in xtt.iterrows()]
             exon_sets = list(zip(exon_coords[:-1], exon_coords[1:]))
+            
+            assert min([x[1][1] - x[0][2] for x in exon_sets]) >= 0
             xtable[transid] = exon_coords
             jtable[transid] = exon_sets
             btypes[biotype].update(set(exon_coords))
@@ -73,29 +83,39 @@ def load_gtf(app, args):
             for x in btypes[b]:
                 exon2biotype[x].add(b)
 
-        
+        # now associate unique biotypes & calculate exon naems
         for transid in jtable:
             for jstart, jstop in jtable[transid]:
-                print(transid)
-                print(' - ', jstart,exon2biotype[jstart])
-                print(' - ', jstop,exon2biotype[jstop])
-            
-        exit()
-    exons = exons[exons['gene_name'].str.contains('Sox17')]
-    print(exons.head(39))
-    exit()
+                bt_start = exon2biotype[jstart]
+                bt_stop = exon2biotype[jstop]
+                if len(bt_start) == 1 and len(bt_stop) == 1:
+                    # I assume this is the case - but if I'm mistaken...
+                    assert bt_start == bt_stop
+                    
+                if len(bt_start) == 1:
+                    jat = list(bt_start)[0]
+                elif len(bt_stop) == 1:
+                    jat = list(bt_stop)[0]
+                else:
+                    continue
+                jname = '%s_%s_%s' % (jstart[0], jstart[2]+1, jstop[1]-1)
+                junction_effect_table.append([gene_id, gene_name, jname, jat])
+                
+    junction_effect_table = pd.DataFrame.from_records(junction_effect_table,
+                                         columns='gene_id gene_name junction_name effect'.split())
 
-    
+    junction_effect_table.to_csv('test.tsv', sep="\t")
     if not args.debug:
+        junction_effect_table.to_sql('gtf_junction_effect', engine, if_exists='replace')
+        exon.to_sql('gtf_exon', engine, if_exists='replace')
         genes.to_sql('gtf_genes', engine, if_exists='replace')
-
         
-    #add indici
-    meta = MetaData()
-    meta.reflect(engine)
-    table = meta.tables['gtf_genes']
-    i = Index('genes.pos', table.c.chr, table.c.start, table.c.stop)
-    i.create(engine)
+        #add indici
+        meta = MetaData()
+        meta.reflect(engine)
+        table = meta.tables['gtf_genes']
+        i = Index('genes.pos', table.c.chr, table.c.start, table.c.stop)
+        i.create(engine)
 
     
 @leip.arg('-d', '--datafile', default='sqlite:///nfj.db')
